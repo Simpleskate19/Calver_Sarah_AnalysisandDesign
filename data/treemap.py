4fdaca89118d61777fce795870255d26b0900b3d
Sarah Calver <sarah@Sarahs-iMac.local>
2019-12-16 05:00:28 -0500

    Putting in the Treemap Graph

---
+++ data/treemap.py
@@ -0,0 +1,20 @@
+#libraries
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import squarify # pip install squarify (algorithm for treemap)&lt;/pre&gt;
+ 
+# Create a dataset:
+my_values=[i**3 for i in range(1,100)]
+ 
+# create a color palette, mapped to these values
+cmap = matplotlib.cm.Blues
+mini=min(my_values)
+maxi=max(my_values)
+norm = matplotlib.colors.Normalize(vmin=mini, vmax=maxi)
+colors = [cmap(norm(value)) for value in my_values]
+ 
+# Change color
+squarify.plot(sizes=my_values, alpha=.8, color=colors )
+plt.axis('off')
+plt.show()
